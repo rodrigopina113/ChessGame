e28900fb7b028f07b06e4013288ee11f947cfcaf
rodrigopina113 <rodrigo.pina113@gmail.com>
2025-06-22 17:14:28 +0100

    Intro e CutScene

---
+++ Assets/Scripts/NextLevelLoader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Video;   // ← certifique-se de ter este using
+
+public static class NextLevelLoader
+{
+    // nome da cena a carregar após a cutscene
+    public static string sceneName;
+
+    // o VideoClip que a cutscene deve reproduzir
+    public static VideoClip cutsceneClip;
+}
+
